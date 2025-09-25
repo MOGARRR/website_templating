2e2aa73dc1ffe4ced6c52b96daa9aa06c56a9599
Anthony Alicea <alicea.9a@gmail.com>
2025-09-25 13:07:23 -0600

    Feat: Create basic nav menu component

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const NavMenu = () => {
   return (
-    <div>NavMenu</div>
-  )
-}
+    <>
+      <div className="flex ">
+        <div className="bg-base-100 fixed top-0 w-7/9 h-screen flex flex-col select-none z-10 ">
+          <div className="flex flex-col h-full text-5xl">
+            <div className=" w-full p-3 border-b-2">Duck Season</div>
+            <Link href={{ pathname: "/" }} className=" m-1  w-full p-3">
+              Home
+            </Link>
+            <Link
+              href={{ pathname: "/about" }}
+              className=" m-1  w-full p-3 "
+            >
+              About
+            </Link>
+            <Link
+              href={{ pathname: "/store" }}
+              className=" m-1  w-full p-3"
+            >
+              Store &gt;
+              <ul className="text-3xl">
+                <li className="m-4">Category 1</li>
+                <li className="m-4">Category 2</li>
+                <li className="m-4">Category 3</li>
 
-export default NavMenu
+              </ul>
+            </Link>
+            <Link
+              href={{ pathname: "/contact" }}
+              className=" m-1  w-full p-3"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavMenu;
